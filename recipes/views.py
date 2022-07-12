87a7572c08029e6ac66cca13c05e260391417857
Rhuan Costanzo <rhuan@cooxupe.com>
2022-07-12 10:37:24 -0300

    categoria-null

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,30 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
+from .models import Recipe
+
 
 def home(request):
-    return render(request,'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for i in range(10)],
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
     })
 
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+    })
+
+
 def recipe(request, id):
-    return render(request,'recipes/pages/recipe-view.html', context={
+    
+    return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
         'is_detail_page': True,
     })
-
-
